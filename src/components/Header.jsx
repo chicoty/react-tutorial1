6726f6a737d450cb17b8407c4db379dd4fa5424e
chicoty <chko22003@g.nihon-u.ac.jp>
2023-10-21 22:36:04 +0900

    dove.pngの追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
             <div className="hero-body">
                 <div className="container">
                     <h1>Birdwatching</h1>
-                    <img src="src\components\dove.png" alt="a simple dove logo"/>
+                    <img src="dove.png" alt="a simple dove logo"/>
                 </div>
             </div>
         </header>
