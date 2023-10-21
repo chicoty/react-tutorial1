77a3399d7a22798095b5d3ed4415478270efe112
chicoty <chko22003@g.nihon-u.ac.jp>
2023-10-21 22:52:09 +0900

    Galleryの追加

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,6 +5,7 @@ export default function Main() {
         <main>
             <section className="section">
                 <div className="container">
+                    <Gallery />
                     <h2>Welcome</h2>
                     <p>Welcome to our fake birdwatching site. If this were a real site, it would be the ideal place to come to learn more about birdwatching, whether you are a beginner looking to learn how to get into birding, or an expert wanting to share ideas, tips, and photos with other like-minded people.</p>
 
