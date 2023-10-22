54dd4fbd1025ec6bc5c255f6d5ec76c03ce06272
chicoty <chko22003@g.nihon-u.ac.jp>
2023-10-22 20:58:42 +0900

    内容の変更

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,3 @@
-import Gallery from "./Gallery";
-
 export default function Main() {
     return (
         <main>
@@ -12,10 +10,10 @@ export default function Main() {
 
                     <h2>Favourite photos</h2>
 
-                    <a href="favorite-1.jpg"><img src="http://localhost:5173/src/components/images/favorite-1_th.jpg" alt="Small black bird, black claws, long black slender beak, links to larger version of the image"/></a>
-                    <a href="favorite-2.jpg"><img src="http://localhost:5173/src/components/images/favorite-2_th.jpg" alt="Top half of a pretty bird with bright blue plumage on neck, light colored beak, blue headdress, links to larger version of the image"/></a>
-                    <a href="favorite-3.jpg"><img src="http://localhost:5173/src/components/images/favorite-3_th.jpg" alt="Top half of a large bird with white plumage, very long curved narrow light colored break, links to larger version of the image"/></a>
-                    <a href="favorite-4.jpg"><img src="http://localhost:5173/src/components/images/favorite-4_th.jpg" alt="Large bird, mostly white plumage with black plumage on back and rear, long straight white beak, links to larger version of the image"/></a>
+                    <a href="favorite-1.jpg"><img src="http://localhost:5173/src/components/images/favorite-1_th.jpg" alt="Small black bird, black claws, long black slender beak, links to larger version of the image" /></a>
+                    <a href="favorite-2.jpg"><img src="http://localhost:5173/src/components/images/favorite-2_th.jpg" alt="Top half of a pretty bird with bright blue plumage on neck, light colored beak, blue headdress, links to larger version of the image" /></a>
+                    <a href="favorite-3.jpg"><img src="http://localhost:5173/src/components/images/favorite-3_th.jpg" alt="Top half of a large bird with white plumage, very long curved narrow light colored break, links to larger version of the image" /></a>
+                    <a href="favorite-4.jpg"><img src="http://localhost:5173/src/components/images/favorite-4_th.jpg" alt="Large bird, mostly white plumage with black plumage on back and rear, long straight white beak, links to larger version of the image" /></a>
                 </div>
             </section>
         </main>
